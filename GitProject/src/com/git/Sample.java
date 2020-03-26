a8d5a3c0ecb751aa9e60373aa00f1c1d073951aa
Sandhiya <sandhiyamcet2015@gmail.com>
2020-03-26 18:28:28 +0530

    file project

---
+++ GitProject/src/com/git/Sample.java
@@ -0,0 +1,9 @@
+package com.git;
+
+public class Sample {
+public static void main(String[] args) {
+	System.out.println("This is a Sample git operation");
+	
+	System.out.println("This is the changes made in GitHub")
+}
+}
