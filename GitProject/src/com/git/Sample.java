e1209b87272e63f46acf40d1a71f5cecdf2b991d
Sandhiya27 <62693530+Sandhiya27@users.noreply.github.com>
2020-03-26 18:34:37 +0530

    Update Sample.java

--- GitProject/src/com/git/Sample.java
+++ GitProject/src/com/git/Sample.java
@@ -4,6 +4,9 @@ public class Sample {
 public static void main(String[] args) {
 	System.out.println("This is a Sample git operation");
 	
-	System.out.println("This is the changes made in GitHub")
+	System.out.println("This is the changes made in GitHub");
+	
+	System.out.println("After pushing operation");
+	
 }
 }
